da3a31f490398329c005cbf2090b063be8bc5d0a
Dmitriy Lobanov <dvmald@icloud.com>
2021-08-05 13:37:24 +0300

    Initial commit

---
+++ AlamofireDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireDemo
+//
+//  Created by Dmitrii Lobanov on 05.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
